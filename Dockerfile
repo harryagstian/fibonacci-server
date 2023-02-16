f35fcad90e2ef13ed11551df0f7bb11a9c1dfaaf
Harry Agustian <harry.agustian@autobahn-security.com>
2023-02-16 18:16:43 +0700

    Init commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:16.13
+
+WORKDIR /app
+
+COPY package*.json pnpm-lock.yaml ./
+
+RUN curl -f https://get.pnpm.io/v6.16.js | node - add --global pnpm@6
+
+COPY . .
+
+RUN pnpm install
+
+CMD ["pnpm", "run", "run"]
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+
+const getRandomInt = () => {
+  const max = 1000000000;
+  return Math.floor(Math.random() * max);
+}
+
+const dumbSort = () => {
+  let arrayLength = process.env.ARRAY_LENGTH || 10000000
+  let arr = Array.from({ length: arrayLength }, () => getRandomInt())
+  arr.sort()
+
+  return arr.length
+}
+
+app.get('/', function (req, res) {
+  console.log("traffic")
+
+  res.send(
+    {
+      length: dumbSort(),
+      date: new Date()
+    }
+  )
+})
+
+const port = process.env.PORT || 8080
+app.listen(port)
